99607f4de621dcaf8bf4dd7046dec1dd069c4e3f
daniel3scripts <paul.lugo@tecsup.edu.pe>
2022-06-19 04:53:29 -0500

    comit02

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Lab13Database.ViewModels
+{
+    public class BaseViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetValue<T>(ref T backingField, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingField, value))
+            {
+                return;
+            }
+
+            backingField = value;
+            OnPropertyChanged(propertyName);
+        }
+
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Lab13Database.Models;
+using Lab13Database.Services;
+
+namespace Lab13Database.ViewModels
+{
+    public class MainViewModel :BaseViewModel
+    {
+        #region Attributes
+        private ObservableCollection<MenuItemViewModel> menu;
+        #endregion Attributes
+
+        #region Properties
+        public ObservableCollection<MenuItemViewModel> Menu
+        {
+            get { return this.menu; }
+            set { SetValue(ref this.menu, value); }
+        }
+        #endregion Properties
+
+        #region Constructor
+        public MainViewModel()
+        {
+            this.LoadMenu();
+
+            //this.SaveArtistasList();
+        }
+        #endregion Constructor
+
+        #region Methods
+        private void LoadMenu()
+        {
+            this.Menu = new ObservableCollection<MenuItemViewModel>();
+
+            this.Menu.Clear();
+            this.Menu.Add(new MenuItemViewModel { Id = 1, Option = "Crear" });
+            this.Menu.Add(new MenuItemViewModel { Id = 2, Option = "Lista de Registros" });
+        }
+        #endregion Methods
+
+        DBDataAccess<Artista> dataService = new DBDataAccess<Artista>();
+        private void SaveArtistasList()
+        {
+            var artistas = new List<Artista>()
+            {
+                new Artista{ Nombre = "Arjona" },
+                new Artista{ Nombre = "Luismi" },
+                new Artista{ Nombre = "Kalimba" }
+            };
+
+            dataService.SaveList(artistas);
+        }
+
+
+    }
+}
